2cc5de8fa435d8ffae6f41e4e758b65796ae0de8
Axel Jimenez <axely7@hotmail.com>
2023-05-10 15:08:26 -0600

    control props alternativo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -9,32 +9,17 @@ export const useShoppingCart = () => {
     
       const onProductCountChange = ({ count, product }: onChangeArgs) => {
         setShoppingCart((oldShoppingCart) => {
-          const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-            ...product,
-            count: 0,
-          };
-    
-          if (Math.max(productInCart.count + count, 0) > 0) {
-            productInCart.count += count;
-            return {
-              ...oldShoppingCart,
-              [product.id]: productInCart,
-            };
+       
+            console.log(count)
+          if (count === 0) {
+            // delete oldShoppingCart[product.id];
+            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+            return rest;
           }
-    
-          // Borrar el producto
-          const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-          return rest;
-    
-          // if (count === 0) {
-          //   // delete oldShoppingCart[product.id];
-          //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-          //   return rest;
-          // }
-          // return {
-          //   ...oldShoppingCart,
-          //   [product.id]: { ...product, count },
-          // };
+          return {
+            ...oldShoppingCart,
+            [product.id]: { ...product, count },
+          };
         });
       };
 
